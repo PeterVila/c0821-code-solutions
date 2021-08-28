fb8d8a7e091d87bd64bfdcf599cfa3c644a0b409
Peter Vila <peterdvila@gmail.com>
2021-08-27 17:53:39 -0700

    Fixed reverseString for 1 loop instead of 3 LOOPS

--- string-manipulation-1/src/reverse-word.js
+++ string-manipulation-1/src/reverse-word.js
@@ -1,38 +1,22 @@
 /* exported reverseWord */
 function reverseWord(word) {
-  var reverseArr = [];
-  var reverseOutput = [];
-  var finalOutput = [];
-  var finalWord = '';
-  for (var i = 0; i < word.length; i++) {
-    reverseArr.push(word[i]);
-  }
-  for (var j = reverseArr.length; j >= 0; j--) {
-    reverseOutput.push(reverseArr[j]);
-  }
-  for (var k = 0; k < reverseOutput.length; k++) {
-    if (typeof reverseOutput[k] === 'string') {
-      finalOutput.push(reverseOutput[k]);
+  var reverse = '';
+  for (var i = word.length; i >= 0; i--) {
+    if (word[i] !== undefined) {
+      reverse += word[i];
     }
   }
-  finalWord = finalOutput.join('');
-  return finalWord;
+  return reverse;
 }
 
 /*
-input: a single word as a string
-output: the input word in reverse order
+Input: 1 string
+Output: Input but reversed
 
-create storage for input (reverseArr);
-create a storage for the reverseArr to be put backwards.
+Make a container for the string result
+Go through the input string but starting from the END to the beginning
+If character is not defined, add it  to the string result
 
-created a special storage finalOutput to only work with string values
-created a storage for the final word output
+Return the string result
 
-look at each character in our input and put them into a storage(1)
-look at every character in storage(1) counting BACKWARDS till 0, and put them into storage(2)
-
-count through every index in storage(2) and only add string values to storage(3)
-
-With storage(3), join together every value into 1 full word.
 */
