99dce30a46de74e2180ca7a93d1dd7456f82281f
Peter Vila <peterdvila@gmail.com>
2021-08-30 09:57:28 -0700

    Changed word.length to word.length-1 so I can remove the conditional for falsy values

--- string-manipulation-1/src/reverse-word.js
+++ string-manipulation-1/src/reverse-word.js
@@ -1,10 +1,8 @@
 /* exported reverseWord */
 function reverseWord(word) {
   var reverse = '';
-  for (var i = word.length; i >= 0; i--) {
-    if (word[i] !== undefined) {
-      reverse += word[i];
-    }
+  for (var i = word.length - 1; i >= 0; i--) {
+    reverse += word[i];
   }
   return reverse;
 }
