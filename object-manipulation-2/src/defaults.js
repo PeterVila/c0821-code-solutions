00391285127422e6c1e7894ac1876e39a66585b3
Peter Vila <peterdvila@gmail.com>
2021-08-30 15:19:43 -0700

    Failed 1 test left

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,23 +1,37 @@
 /* exported defaults */
 function defaults(target, source) {
+  var sourceKeys = []; //has Our Values from source
+  
 
-  var sourceKeys = [];
-  for (var key in source) {
+  for (var key in source){
     sourceKeys.push(key);
   }
-  var targetKeys = [];
-  for (var key2 in target) {
-    targetKeys.push(key2);
+  for (var i = 0; i < sourceKeys.length; i++){
+    for(var key2 in target){
+      if (key2 === sourceKeys[i]){
+          delete source.key2;
+      }
+    }
   }
 
+  for (var key3 in source){
+    if(key3 !== target[key3]){
+      target[key3] = source[key3];
+    }
+  }
+
+  console.log("sourceKeys:",sourceKeys);
+  console.log("source:", source);
+  console.log("target:", target);
 }
 /*
-Input: Takes in 2 objects
-Output: Modiefies the 1st object for missing properties (ignore same properties)
+Input: Takes in a object to be editted. Takes in an object to be looked over
+Output: A new object that adds the missing properties our to-be-edited object with the source object.
 
-put properties of source in a list
-put properties of target in a list
-empty list for different values
+Create a container for our source property Keys.
+Create a container for keys that our target is missing
 
-Go through each value in source list to find equal values in target list, put
+Go through each value in source object and grab the keys and put them into sourceKeys
+Go through each value in our sourceKeys list and if the value matches it, delete from source
+
 */
