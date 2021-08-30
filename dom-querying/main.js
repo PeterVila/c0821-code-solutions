94f37105e9d0afda45002b7b05bcc2866239ea4e
Peter Vila <peterdvila@gmail.com>
2021-08-30 10:44:31 -0700

    Completed dom-querying exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,12 @@
+console.log('hello, world');
+console.log('document.querySelector():', document.querySelector('h1'));
+console.dir(document.querySelector('h1'));
+
+console.log("document.querySelector('#explanation'):", document.querySelector('#explanation'));
+console.dir(document.querySelector('#explanation'));
+
+console.log("document.querySelector('.hint'):", document.querySelector('.hint'));
+console.dir(document.querySelector('.hint'));
+
+console.log("document.querySelectorAll('p'):", document.querySelectorAll('p'));
+console.log("document.querySelectorAll('.example-link'):", document.querySelectorAll('.example-link'));
