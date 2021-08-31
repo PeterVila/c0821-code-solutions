94081c8da66e84a003f1885e7e52b71eb019f314
Peter Vila <peterdvila@gmail.com>
2021-08-31 16:56:24 -0700

    Removed boolean variable and just worked without conditionals

--- javascript-modal/script.js
+++ javascript-modal/script.js
@@ -1,22 +1,19 @@
-var noPopup = true;
 var $bluebutton = document.querySelector('.blue-button');
 var $redbutton = document.querySelector('.red-button');
 var $modal = document.querySelector('.modal');
 var $background = document.querySelector('.background-default');
 
 $bluebutton.addEventListener('click', function () {
-  if (noPopup) {
-    $background.className = 'background-dim';
-    $modal.className = 'modal justify-center';
-  }
+  $background.className = 'background-dim';
+  $modal.className = 'modal justify-center';
+
 });
 
 $redbutton.addEventListener('click', function () {
-  if (noPopup) {
-    $background.className = 'background-default';
-    $modal.className = 'modal hidden justify-center';
-  }
-});
+  $background.className = 'background-default';
+  $modal.className = 'modal hidden justify-center';
+}
+);
 
 /*
 Create a variable to indicate if there is no pop up
