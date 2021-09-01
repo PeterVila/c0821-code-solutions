a1b61b1fd1f382f0e14e2039169d65ed58fcf289
Peter Vila <peterdvila@gmail.com>
2021-09-01 11:19:53 -0700

    Completed javascript-forms exercise.

--- javascript-forms/form-events.js
+++ javascript-forms/form-events.js
@@ -1,7 +1,6 @@
 var $name = document.forms[0].elements[0];
 var $email = document.forms[0].elements[1];
 var $message = document.forms[0].elements[2];
-var $form = document.querySelector('#contact-form');
 
 $name.addEventListener('focus', handleFocus);
 $name.addEventListener('blur', handleBlur);
@@ -15,8 +14,6 @@ $message.addEventListener('focus', handleFocus);
 $message.addEventListener('blur', handleBlur);
 $message.addEventListener('input', handleInput);
 
-$form.addEventListener('submit', formSubmit);
-
 function handleFocus(event) {
   console.log('focus event fired');
   console.log('event.target.name', event.target.name);
@@ -28,15 +25,4 @@ function handleBlur(event) {
 
 function handleInput(event) {
   console.log('Value of ' + event.target.name + ':', event.target.value);
-}
-
-function formSubmit(event) {
-  event.preventDefault();
-  var values = {
-    name: $form.elements.name.value,
-    email: $form.elements.email.value,
-    message: $form.elements.message.value
-  };
-  console.log(values);
-  $form.reset();
 }
