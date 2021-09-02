659e0111cc64242f77a4afbb3000213d46ad8102
Peter Vila <peterdvila@gmail.com>
2021-09-01 20:54:44 -0700

    Cleaan code for object-manipulation-2

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,35 +1,17 @@
 /* exported defaults */
 function defaults(target, source) {
-  var sourceKeys = []; //
-
   for (var key in source) {
-    sourceKeys.push(key);
-  }
-  for (var i = 0; i < sourceKeys.length; i++) {
-    for (var key2 in target) {
-      if (key2 === sourceKeys[i]) {
-        delete source[key2];
-      }
-    }
-  }
-
-  for (var key3 in source) {
-    if (key3 !== target[key3]) {
-      target[key3] = source[key3];
+    if (target[key] === undefined) {
+      target[key] = source[key];
     }
   }
 }
 /*
-Input: Takes in a object to be editted. Takes in an object to be looked over
-Output: A new object that adds the missing properties our to-be-edited object with the source object.
+input:
+  - JavaScript Object (target)
+  - JavaScript Object (target)
+Output:
+  -Modifies the target object to include missing source
 
-Create a container for our source property Keys.
-
-Go through each value in source object and grab the keys and put them into sourceKeys
-Go through each value in our sourceKeys list and if the value matches it, delete from source
-
-for each property in our source
-  if that property is not equal to target object,
-    Add that property from our source to target
-
+Go through every value in target
 */
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,14 +1,11 @@
 /* exported pick */
 
 function pick(source, keys) {
-
   var output = {};
-
-  for (var key in source) {
-
-    if (source[key] !== undefined) {
-      for (var i = 0; i < keys.length; i++) {
-        if (key === keys[i]) {
+  for (var i = 0; i < keys.length; i++) {
+    for (var key in source) {
+      if (key === keys[i]) {
+        if (source[key] !== undefined) {
           output[key] = source[key];
         }
       }
