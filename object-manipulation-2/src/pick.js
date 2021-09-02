7434a6e3fb6766dad86a5c6f29361ec955fb8bd7
Peter Vila <peterdvila@gmail.com>
2021-09-02 11:16:47 -0700

    Simpliefied pick.js

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -3,12 +3,8 @@
 function pick(source, keys) {
   var output = {};
   for (var i = 0; i < keys.length; i++) {
-    for (var key in source) {
-      if (key === keys[i]) {
-        if (source[key] !== undefined) {
-          output[key] = source[key];
-        }
-      }
+    if (source[keys[i]] !== undefined) {
+      output[keys[i]] = source[keys[i]];
     }
   }
   return output;
