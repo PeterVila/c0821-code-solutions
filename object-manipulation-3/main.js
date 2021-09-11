5444710c893797b7e17017af3c2ed5ca3a12a886
Peter Vila <peterdvila@gmail.com>
2021-09-10 17:57:07 -0700

    Completed card game. Was unable to create tiebreaker

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,116 +1,63 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
-var players = [
-  { name: 'player1', hand: [] },
-  { name: 'player2', hand: [] },
-  { name: 'player3', hand: [] },
-  { name: 'player4', hand: [] }
-];
-var deck = [];
-function createDeck(suit) {
-  deck.push({ rank: 'Ace', suit: suit });
-  for (var i = 2; i <= 10; i++) {
-    deck.push({ rank: i, suit: suit });
-  }
-  deck.push({ rank: 'Jack', suit: suit });
-  deck.push({ rank: 'Queen', suit: suit });
-  deck.push({ rank: 'King', suit: suit });
-}
-createDeck('clubs');
-createDeck('diamonds');
-createDeck('hearts');
-createDeck('spades');
 
-console.log('deck:', deck);
+cardGame(['Jim', 'Bob', 'Eric', 'Jim'], 3);
+function cardGame(playerList, cardsPerHand) {
+  var players = [];
+  var deck = [];
 
-var shuffledDeck = _.shuffle(deck);
-for (var i = 0; i < players.length; i++) {
-  for (var x = 0; x < 1; x++) {
-    players[i].hand.push(shuffledDeck[x]);
-    shuffledDeck.splice(x, 1);
-    players[i].hand.push(shuffledDeck[x]);
-    shuffledDeck.splice(x, 1);
+  for (var l = 0; l < playerList.length; l++) {
+    players.push({
+      name: playerList[l],
+      hand: [],
+      score: 0
+    });
   }
-}
-console.log('shuffledDeck after giving cards out:', shuffledDeck.length);
-console.log('players:', players);
-var scoresInOrder = [];
-for (var z = 0; z < players.length; z++) {
-  for (var key in players[z].hand) {
-    scoresInOrder.push(players[z].hand[key].rank);
-  }
-}
-var scoresValue = [];
-for (var m = 0; m < scoresInOrder.length; m++) {
-  if (scoresInOrder[m] === 'Ace') {
-    scoresValue.push(11);
-  } else if (scoresInOrder[m] === 'Jack' || scoresInOrder[m] === 'Queen' || scoresInOrder[m] === 'King') {
-    scoresValue.push(10);
-  } else if (scoresInOrder[m] === 2) {
-    scoresValue.push(2);
-  } else if (scoresInOrder[m] === 3) {
-    scoresValue.push(3);
-  } else if (scoresInOrder[m] === 4) {
-    scoresValue.push(4);
-  } else if (scoresInOrder[m] === 5) {
-    scoresValue.push(5);
-  } else if (scoresInOrder[m] === 6) {
-    scoresValue.push(6);
-  } else if (scoresInOrder[m] === 7) {
-    scoresValue.push(7);
-  } else if (scoresInOrder[m] === 8) {
-    scoresValue.push(8);
-  } else {
-    scoresValue.push(9);
-  }
-}
-var playerByIndex = [];
-for (var j = 0; j < scoresValue.length; j++) {
-  playerByIndex.push(scoresValue[j] + scoresValue[j++]);
-}
-console.log('Players 1-4:', playerByIndex);
-var final1 = playerByIndex[0];
-var final2 = playerByIndex[1];
-var final3 = playerByIndex[2];
-var final4 = playerByIndex[3];
-if (final1 > final2 && final1 > final3 && final1 > final4) {
-  console.log('%cPlayer1 Wins!', 'color: cyan; font-size: 15px');
-} else if (final2 > final1 && final2 > final3 && final2 > final4) {
-  console.log('%cPlayer2 Wins!', 'color: cyan; font-size: 15px');
-} else if (final3 > final1 && final3 > final2 && final3 > final4) {
-  console.log('%cPlayer3 Wins!', 'color: cyan; font-size: 15px');
-} else if (final4 > final1 && final4 > final2 && final4 > final3) {
-  console.log('%cPlayer4 Wins!', 'color: cyan; font-size: 15px');
-} else if (final1 === final2 && final1 === final3 && final1 === final4) {
-  console.log('%cEveryone Tied!', 'color: cyan; font-size: 15px');
-} else if ((final1 && final2) > (final3 && final4)) {
-  console.log('Player1 and Player2 Tied!');
-  tieBreaker('Player 1', 'Player2');
-} else if ((final1 && final3) > (final2 && final4)) {
-  console.log('Player1 and Player3 Tied!');
-  tieBreaker('Player 1', 'Player3');
-} else if ((final1 && final4) > (final2 && final3)) {
-  console.log('Player1 and Player4 Tied!');
-  tieBreaker('Player 1', 'Player4');
-} else if ((final2 && final3) > (final1 && final4)) {
-  console.log('Player2 and Player3 Tied!');
-  tieBreaker('Player 2', 'Player3');
-} else if ((final2 && final4) > (final1 && final3)) {
-  console.log('Player2 and Player4 Tied');
-  tieBreaker('Player 2', 'Player4');
-} else if ((final3 && final4) > (final1 && final2)) {
-  console.log('Player3 and Player4 Tied!');
-  tieBreaker('Player 3', 'Player4');
-}
-function tieBreaker(person1, person2) {
-  console.log(person1 + ' and ' + person2 + ' pull another card!');
-  var tie1 = _.random(2, 11);
-  var tie2 = _.random(2, 11);
-  if (tie1 > tie2) {
-    console.log(person1 + ' wins! Their card is worth ' + tie1 + ' points');
-    console.log(person2 + ' pulled a card worth ' + tie2 + ' points.');
-  } else {
-    console.log(person2 + ' wins! Their card is worth ' + tie2 + ' points');
-    console.log(person1 + ' pulled a card worth ' + tie1 + ' points.');
 
+  var suits = ['clubs', 'diamonds', 'hearts', 'spades'];
+  var ranks = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'King', 'Queen', 'Jack'];
+  for (var i = 0; i < suits.length; i++) {
+    for (var x = 0; x < ranks.length; x++) {
+      deck.push({
+        suits: suits[i],
+        rank: ranks[x]
+      });
+    }
+  }
+  var shuffledDeck = _.shuffle(deck);
+
+  for (var z = 0; z < players.length; z++) {
+    for (var m = 0; m < cardsPerHand; m++) {
+      players[z].hand.push(shuffledDeck.pop());
+    }
+    for (var p = 0; p < players[z].hand.length; p++) {
+      console.log(players[z].hand[p].rank);
+      if (players[z].hand[p].rank === 'Ace') {
+        players[z].score += 11;
+      } else if (players[z].hand[p].rank === 'King' || players[z].hand[p].rank === 'Queen' || players[z].hand[p].rank === 'Jack') {
+        players[z].score += 10;
+      } else {
+        players[z].score += players[z].hand[p].rank;
+      }
+    }
+  }
+  console.log(players);
+  console.log(shuffledDeck);
+
+  // TIE BREAKER
+  // Populate a new array
+  console.log(players[0].score, players[1].score);
+
+  var tieBreaker = [];
+  for (var g = 0; g < players.length; g++) {
+    tieBreaker.push(players[g].score);
+  }
+  console.log(tieBreaker);
+  var highestValue = Math.max(...tieBreaker);
+  console.log(highestValue);
+
+  for (var e = 0; e < players.length; e++) {
+    if (players[e].score === highestValue) {
+      console.log(players[e].name + ' has won!');
+    }
   }
 }
