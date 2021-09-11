615044243ac329b592a70d40819e2c9a2d089121
Peter Vila <peterdvila@gmail.com>
2021-09-11 16:12:24 -0700

    Finished card game and both optional exercises

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,10 +1,8 @@
-console.log('Lodash is loaded:', typeof _ !== 'undefined');
-
+// console.log('Lodash is loaded:', typeof _ !== 'undefined');
 cardGame(['Jim', 'Bob', 'Eric', 'Jim'], 3);
 function cardGame(playerList, cardsPerHand) {
   var players = [];
   var deck = [];
-
   for (var l = 0; l < playerList.length; l++) {
     players.push({
       name: playerList[l],
@@ -12,7 +10,6 @@ function cardGame(playerList, cardsPerHand) {
       score: 0
     });
   }
-
   var suits = ['clubs', 'diamonds', 'hearts', 'spades'];
   var ranks = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'King', 'Queen', 'Jack'];
   for (var i = 0; i < suits.length; i++) {
@@ -24,7 +21,6 @@ function cardGame(playerList, cardsPerHand) {
     }
   }
   var shuffledDeck = _.shuffle(deck);
-
   for (var z = 0; z < players.length; z++) {
     for (var m = 0; m < cardsPerHand; m++) {
       players[z].hand.push(shuffledDeck.pop());
@@ -39,19 +35,34 @@ function cardGame(playerList, cardsPerHand) {
       }
     }
   }
-
-  // TIE BREAKER
-  // Populate a new array
-
+  // TIE BREAKER, Logging original players
+  if (players.length === playerList.length) {
+    console.log('Starting Players:', players);
+  }
   var tieBreaker = [];
   for (var g = 0; g < players.length; g++) {
     tieBreaker.push(players[g].score);
   }
   var highestValue = Math.max(...tieBreaker);
-
   for (var e = 0; e < players.length; e++) {
-    if (players[e].score === highestValue) {
-      console.log(players[e].name + ' has won!');
+
+    if (players[e].score !== highestValue) {
+      players.splice(e, 1);
+      e--; // Without this, it doesn't target the last e
     }
+  }
+  console.log('Highest score player(s): ', players);
+  if (players.length > 1) {
+    console.log('Tiebreaker between ' + players.length + ' players!');
+  }
+  if (players.length === 1) {
+    console.log('%c' + players[0].name + ' is the winner!', 'color:gold');
+  } else {
+    // Bring players names into a new array
+    var playersTied = [];
+    for (var c = 0; c < players.length; c++) {
+      playersTied.push(players[g].name);
+    }
+    cardGame(playersTied, cardsPerHand);
   }
 }
