e39e1fbd28671d99a5c4b0044ce49eea071a9ffd
Peter Vila <peterdvila@gmail.com>
2021-09-14 16:47:32 -0700

    intersection.js now checks for duplicates

--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -4,12 +4,7 @@ function intersection(first, second) {
   for (var i = 0; i < first.length; i++) {
     for (var x = 0; x < second.length; x++) {
       if (first[i] === second[x]) {
-        for (var z = 0; z < output.length; z++) {
-          if (first[x] === output[z]) {
-            i++;
-          }
-        }
-        if (first[i] !== undefined) {
+        if (!output.includes(first[i])) {
           output.push(first[i]);
         }
       }
@@ -18,18 +13,4 @@ function intersection(first, second) {
   return output;
 }
 
-// Continueing to find multiple same values.
-// console.log(intersection([2,1], [2,3,2,1,1]))
-
-/*
-Input: Takes in 2 lists
-Output: An array of the value if the value matches.
-
-Create an empty container for our new list
-Go through every value of our first list
-    At each value, go through every value of our second list
-        If the current value of first List matches any values in second loop,
-            add the the current value to our output
-
-return output
-*/
+// Don't add if that value already exists in output.
