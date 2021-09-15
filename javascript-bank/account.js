75b8374d4152f5b43c3c1e70e01d0b0a2db2a636
Peter Vila <peterdvila@gmail.com>
2021-09-15 10:31:42 -0700

    Removed comments and test code

--- javascript-bank/account.js
+++ javascript-bank/account.js
@@ -1,46 +1,37 @@
 /* exported Account */
 
-
-
-function Account(number, holder){
-    this.number = number;
-    this.holder = holder;
-    this.transactions = [];
+function Account(number, holder) {
+  this.number = number;
+  this.holder = holder;
+  this.transactions = [];
 }
 
-Account.prototype.deposit = function(amount){
-    // console.log("deposit amount: ", amount);
-    // console.log("deposit amount is integer:", Number.isInteger(amount))
-    if (amount > 0 && Number.isInteger(amount)){
-        this.transactions.push(new Transaction('deposit', amount))
-        return true;
+Account.prototype.deposit = function (amount) {
+  if (amount > 0 && Number.isInteger(amount)) {
+    this.transactions.push(new Transaction('deposit', amount));
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Account.prototype.withdraw = function (amount) {
+  if (amount > 0 && Number.isInteger(amount)) {
+    this.transactions.push(new Transaction('withdrawal', amount));
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Account.prototype.getBalance = function () {
+  var balance = 0;
+  for (var i = 0; i < this.transactions.length; i++) {
+    if (this.transactions[i].type === 'withdrawal') {
+      balance -= this.transactions[i].amount;
     } else {
-     return false;
+      balance += this.transactions[i].amount;
     }
-}
-
-Account.prototype.withdraw = function(amount){
-    // console.log("withdraw amount: ", amount);
-    // console.log("withdraw amount is integer:", Number.isInteger(amount))
-    if (amount > 0 && Number.isInteger(amount)){
-        this.transactions.push(new Transaction('withdrawal', amount))
-        return true;
-    } else {
-        return false;
-    }
-}
-
-Account.prototype.getBalance = function(){
-    var balance = 0;
-    if (this.transactions.length === 0){
-        return 0;
-    }
-    for (var i = 0; i < this.transactions.length; i++){
-        if (this.transactions[i].type === "withdrawal"){
-            balance -= this.transactions[i].amount
-        } else {
-            balance += this.transactions[i].amount
-        }
-    }
-    return balance;
-}
+  }
+  return balance;
+};
--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -1,51 +1,36 @@
 /* exported Bank */
-//Assume Account is already defined, access with Account
+// Assume Account is already defined, access with Account
 
-function Bank(){
-    this.nextAccountNumber = 1; 
-    this.accounts = [] 
+function Bank() {
+  this.nextAccountNumber = 1;
+  this.accounts = [];
 }
 
-Bank.prototype.openAccount = function(holder, balance){
-    if (balance > 0 && Number.isInteger(balance)){
-        var newAccount = new Account(this.nextAccountNumber, holder)
-        newAccount.deposit(balance);
-        this.accounts.push(newAccount);
-        this.nextAccountNumber++
-        return newAccount.number 
-    } else {
-        return null;
-    }
-}
-
-Bank.prototype.getAccount = function(number){
-    for (var z = 0; z < this.accounts.length; z++){
-        if (this.accounts[z].number === number){
-            return this.accounts[z]
-        } 
-    } 
+Bank.prototype.openAccount = function (holder, balance) {
+  if (balance > 0 && Number.isInteger(balance)) {
+    var newAccount = new Account(this.nextAccountNumber, holder);
+    newAccount.deposit(balance);
+    this.accounts.push(newAccount);
+    this.nextAccountNumber++;
+    return newAccount.number;
+  } else {
     return null;
-}
+  }
+};
 
-Bank.prototype.getTotalAssets = function(){
-    var grandTotal = 0;
-    for (var p = 0; p < this.accounts.length; p++){
-        grandTotal += this.accounts[p].getBalance();
+Bank.prototype.getAccount = function (number) {
+  for (var z = 0; z < this.accounts.length; z++) {
+    if (this.accounts[z].number === number) {
+      return this.accounts[z];
     }
-    return grandTotal;
-}
+  }
+  return null;
+};
 
-
-
-//Testing constructors.
-var bank = new Bank()
-var accountNumber = bank.openAccount('Pepe Poo', 1000);
-console.log("var accountNumber:", accountNumber)
-console.log("variable bank:", bank)
-console.log("bank instanceof Bank()", bank instanceof Bank);
-var account = bank.getAccount(1)
-console.log("account instanceof Account", account instanceof Account)
-account.withdraw(300);
-account.getBalance();
-console.log(account)
-console.log("account instance of Account()", account instanceof Account);
+Bank.prototype.getTotalAssets = function () {
+  var grandTotal = 0;
+  for (var p = 0; p < this.accounts.length; p++) {
+    grandTotal += this.accounts[p].getBalance();
+  }
+  return grandTotal;
+};
