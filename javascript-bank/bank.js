bf64604aaf445cbd69e26917c201c8d73524ae0a
Peter Vila <peterdvila@gmail.com>
2021-09-15 09:55:14 -0700

    Completed javascript-bank exercise

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -2,29 +2,23 @@
 //Assume Account is already defined, access with Account
 
 function Bank(){
-    this.nextAccountNumber = 1; //Account number
-    this.accounts = [] //Array of all accounts
+    this.nextAccountNumber = 1; 
+    this.accounts = [] 
 }
 
-//Prototype method to open account, given name and a balance
 Bank.prototype.openAccount = function(holder, balance){
     if (balance > 0 && Number.isInteger(balance)){
-        //Using function Account(number, holder) to create a NEW instance
         var newAccount = new Account(this.nextAccountNumber, holder)
-        //Run the prototype method "deposit" for balance (also signaling type)
         newAccount.deposit(balance);
-        //Pushing this newly made account to our array 
         this.accounts.push(newAccount);
         this.nextAccountNumber++
-        console.log("New Account Objects:", newAccount);
-        return newAccount.number //Returns if successful
+        return newAccount.number 
     } else {
         return null;
     }
 }
 
 Bank.prototype.getAccount = function(number){
-    //Loop through all accounts and finds the matching account
     for (var z = 0; z < this.accounts.length; z++){
         if (this.accounts[z].number === number){
             return this.accounts[z]
@@ -34,11 +28,24 @@ Bank.prototype.getAccount = function(number){
 }
 
 Bank.prototype.getTotalAssets = function(){
-    //Grand total of all bank accounts. Loop through all this.accounts and use the prototype method getBalance() and add it to our grandTotal.
     var grandTotal = 0;
     for (var p = 0; p < this.accounts.length; p++){
-        console.log("getBalance", this.accounts[p].getBalance())
         grandTotal += this.accounts[p].getBalance();
     }
     return grandTotal;
 }
+
+
+
+//Testing constructors.
+var bank = new Bank()
+var accountNumber = bank.openAccount('Pepe Poo', 1000);
+console.log("var accountNumber:", accountNumber)
+console.log("variable bank:", bank)
+console.log("bank instanceof Bank()", bank instanceof Bank);
+var account = bank.getAccount(1)
+console.log("account instanceof Account", account instanceof Account)
+account.withdraw(300);
+account.getBalance();
+console.log(account)
+console.log("account instance of Account()", account instanceof Account);
--- javascript-bank/transaction.js
+++ javascript-bank/transaction.js
@@ -1,8 +1,9 @@
 /* exported Transaction */
 
-//Creates an object with "withdrawal/Deposit" for type and amount we work with.
+//Creates an object constructor function with "withdrawal/Deposit" for type and amount we work with.
 function Transaction(type, amount){
     this.type = type
     this.amount = amount;
 }
+
 
