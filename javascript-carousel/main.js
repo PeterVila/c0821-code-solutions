3a70eff4ae56aa033c463ec26b970ec3a02f2605
Peter Vila <peterdvila@gmail.com>
2021-09-15 21:38:43 -0700

    Added clearInterval whenever an image set was done

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,88 +1,77 @@
 var position = 0;
+var interval = setInterval(nextSlide, 3000);
 
-var interval = setInterval(nextSlide, 3000)
-
-
-function nextSlide(){
-    addPosition();
-    setImg();
-    circleRotation();
-    if (position === 4) {
-      setTimeout(function () {
-        position = 0;
-        setImg();
-        circleRotation()
-      }, 1500)
-    }
+function nextSlide() {
+  addPosition();
+  setImg();
+  circleRotation();
 }
 
-var $clickableDots = document.querySelectorAll('.fa-circle')
-$clickableDots[0].addEventListener('click', function(){
-    position = 0;
-    setImg();
-    circleRotation();
-})
+var $clickableDots = document.querySelectorAll('.fa-circle');
+$clickableDots[0].addEventListener('click', function () {
+  position = 0;
+  setImg();
+});
 
-$clickableDots[1].addEventListener('click', function(){
-    position = 1
-    setImg();
-    circleRotation();
-})
+$clickableDots[1].addEventListener('click', function () {
+  position = 1;
+  setImg();
+});
 $clickableDots[2].addEventListener('click', function () {
-    position = 2
-    setImg();
-    circleRotation();
-})
+  position = 2;
+  setImg();
+});
 $clickableDots[3].addEventListener('click', function () {
-    position = 3
-    setImg();
-    circleRotation();
-})
+  position = 3;
+  setImg();
+});
 $clickableDots[4].addEventListener('click', function () {
-    position = 4
-    setImg();
-    circleRotation();
-})
+  position = 4;
+  setImg();
+});
 
-var photos = ["images/001.png", "images/46.png", "images/007.png", "images/025.png", "images/004.png"]
+var photos = ['images/001.png', 'images/46.png', 'images/007.png', 'images/025.png', 'images/004.png'];
 
-var $image = document.querySelector('img')
+var $image = document.querySelector('img');
 var $leftArrow = document.querySelector('.leftArrow');
-var $rightArrow = document.querySelector('.rightArrow')
+var $rightArrow = document.querySelector('.rightArrow');
 
-
-$rightArrow.addEventListener('click', function(){
-    setImg();
-    circleRotation();
-    addPosition();
-})
-
+$rightArrow.addEventListener('click', function () {
+  setImg();
+  circleRotation();
+  addPosition();
+});
 
 $leftArrow.addEventListener('click', function () {
   setImg();
   circleRotation();
   minusPosition();
-})
+});
 
-function circleRotation(){
-    for (var i = 0; i < $clickableDots.length; i++) {
-      $clickableDots[i].className = "far fa-circle"
-    }
-    $clickableDots[position].className = "fas fa-circle";
+function circleRotation() {
+  for (var i = 0; i < $clickableDots.length; i++) {
+    $clickableDots[i].className = 'far fa-circle';
+  }
+  $clickableDots[position].className = 'fas fa-circle';
 }
 
-function setImg(){
-    $image.setAttribute('src', photos[position])
+function setImg() {
+  $image.setAttribute('src', photos[position]);
+  circleRotation();
+  clearInterval(interval);
+  interval = setInterval(nextSlide, 3000);
 }
 
-function addPosition(){
-    if (position < 4) {
-      position++;
-    }
+function addPosition() {
+  position++;
+  if (position === 5) {
+    position = 0;
+  }
 }
 
-function minusPosition(){
-  if (position > 0) {
-    position--;
+function minusPosition() {
+  position--;
+  if (position === -1) {
+    position = 4;
   }
 }
