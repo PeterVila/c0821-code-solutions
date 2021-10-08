df8dbae2f8a3776b10cc3a40ec975a3bc00fe063
Peter Vila <peterdvila@gmail.com>
2021-10-08 20:02:35 +0000

    Changed nextId to Id, prevented duplicates in gradesArr, by only pushing during post method

--- express-post-json/index.js
+++ express-post-json/index.js
@@ -11,26 +11,22 @@ app.listen(3000, () => {
 
 const gradesArr = [];
 app.get('/api/grades', function (req, res) {
-  for (const key in grades) {
-    gradesArr.push(grades[key]);
-  }
   res.json(gradesArr);
 });
 
 app.use(express.json());
 
 app.post('/api/grades', function (req, res) {
-  res.status(201).json({
+  const gradeObj = {
     course: req.body.course,
     name: req.body.name,
     score: req.body.score,
-    nextId: nextId
-  });
-  grades.nextId = {
-    course: req.body.course,
-    name: req.body.name,
-    score: req.body.score,
-    nextId: nextId
+    id: nextId
   };
+  res.status(201).json(gradeObj);
+  grades.nextId = gradeObj;
   nextId++;
+  for (const key in grades) {
+    gradesArr.push(grades[key]);
+  }
 });
