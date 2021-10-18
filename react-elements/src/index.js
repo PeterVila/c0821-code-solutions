73b17fe9b424d8e18e2c38340670908f0e5cbeb4
Peter Vila <peterdvila@gmail.com>
2021-10-18 22:53:58 +0000

    Completed react-elements exercise

---
+++ react-elements/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = React.createElement('h1', null, 'React is pretty cool 🤩');
+ReactDOM.render(title, document.getElementById('root'));
