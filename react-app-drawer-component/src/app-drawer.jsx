d67689cf69d4d162871fd50b32dd34c05ee8c7f2
Peter Vila <peterdvila@gmail.com>
2021-10-22 07:21:43 +0000

    Created react-app-drawer-component. Clicking on hamburger icon shows menu navigation and dims the rest of the page. Clicking on any links OR the shade closes the drawer component

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -1,1 +1,61 @@
 import React from 'react';
+
+class AppDrawer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isClicked: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isClicked: !prevState.isClicked
+    }));
+  }
+
+  render() {
+    if (!this.state.isClicked) {
+      return (
+        <div>
+        <div>
+            <i onClick={this.handleClick} className="fas fa-bars"></i>
+            <div className="backdrop"></div>
+            <div className="nav-slider hide-nav">
+              <h3>Choose a Game</h3>
+              <ul>
+                <li onClick={this.handleClick}>The Legend Of Zelda</li>
+                <li onClick={this.handleClick}>A Link to the Past</li>
+                <li onClick={this.handleClick}>Ocarina of Time</li>
+                <li onClick={this.handleClick}>The Wind Waker</li>
+                <li onClick={this.handleClick}>Breath of the Wild</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+        <div className="nav-background">
+            <i onClick={this.handleClick} className="fas fa-bars"></i>
+            <div onClick={this.handleClick} className="backdrop"></div>
+            <div className="nav-slider show-nav">
+              <h3>Choose a Game</h3>
+              <ul>
+                <li onClick={this.handleClick}>The Legend Of Zelda</li>
+                <li onClick={this.handleClick}>A Link to the Past</li>
+                <li onClick={this.handleClick}>Ocarina of Time</li>
+                <li onClick={this.handleClick}>The Wind Waker</li>
+                <li onClick={this.handleClick}>Breath of the Wild</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
+}
+
+export default AppDrawer;
